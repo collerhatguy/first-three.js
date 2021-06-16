b95e75eaeb09b5f142575b8899251fcfaa83ac23
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-06-16 18:11:03 -0400

    added resizing for window

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let scene, renderer, cube, camera, line, controls;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const light = new THREE.AmbientLight();
-// const controls = new OrbitControls( camera, renderer.domElement );
-
-const geometry = new THREE.BoxGeometry(2,2,2);
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-
-const lineShape = new THREE.EdgesGeometry(geometry);
-const lineMaterial = new THREE.LineBasicMaterial({ color: "white" });
-const line = new THREE.LineSegments(lineShape, lineMaterial);
-
-scene.add( cube, line, light );
-
-camera.position.z = 5;
-// controls.update();
-
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    
+    const light = new THREE.AmbientLight();
+    
+    const geometry = new THREE.BoxGeometry(2,2,2);
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    cube = new THREE.Mesh( geometry, material );
+    
+    const lineShape = new THREE.EdgesGeometry(geometry);
+    const lineMaterial = new THREE.LineBasicMaterial({ color: "white" });
+    line = new THREE.LineSegments(lineShape, lineMaterial);
+    
+    scene.add( cube, line, light );
+    
+    camera.position.z = 5;
+}
 function animate() {
     requestAnimationFrame( animate );
     // controls.update();
-    cube.rotateX(.01);
-    line.rotateX(.01);
+    cube.rotateX(-.01);
+    line.rotateX(-.01);
+    cube.rotateY(.01);
+    line.rotateY(.01);
+    cube.rotateZ(.01);
+    line.rotateZ(.01);
     renderer.render( scene, camera );
 }
+init();
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+window.addEventListener("resize", onWindowResize())
 animate();
